4c898ac77ab1838e26670c019408cf9a7998ae52
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-02-22 23:18:08 -0800

    added admin route and data dump to a simple in memory databse (array)

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 //import the express module
 import express from 'express';
+const submissions = [];
 const app = express();
 // Set the port number
-const PORT = 4000;
+const PORT = 3010;
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.post("/submit_form", (req, res) => {
-    res.send("form received! (Validation passed)");
+    
+    
+    const submittedform = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        jobtitle: req.body.jobtitle,
+        Company: req.body.Company,
+        LinkedIn: req.body.LinkedIn,
+        Method: req.body.methodofmeeting,
+        Other: req.body.other,
+        message: req.body.message,
+        mailingchecklist: req.body.mailingchecklist,
+        formathtml: req.body["format-html"],
+        formattext: req.body["format-text"],
+    };
+    submissions.push(submittedform);
+    res.redirect(`/confirmation`);
+});
+app.get("/admin", (req, res) => {
+    res.send(submissions);
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/index.html`);
+    res.sendFile(`${import.meta.dirname}/views/index.html`)
 
+});
+
+app.get(`/confirmation`, (req, res) => {
+   res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
 });
 
 app.listen(PORT,() => {
