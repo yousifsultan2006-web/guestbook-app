6bb8269f83d0df7bfad50d031b2b52299f037bc4
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-03-01 23:56:19 -0800

    added ejs, changed app.js to accomodate for it, and added ejs to confirmation to show all form info

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const submissions = [];
 const app = express();
 // Set the port number
 const PORT = 3011;
+
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.post("/submit_form", (req, res) => {
@@ -22,22 +24,26 @@ app.post("/submit_form", (req, res) => {
         mailingchecklist: req.body.mailingchecklist,
         formathtml: req.body["format-html"],
         formattext: req.body["format-text"],
+
+        timestamp: new Date()
     };
-    submissions.push(submittedform);
-    res.redirect(`/confirmation`);
-});
+    submissions.push(submittedform)
+        res.render("confirmation", {submittedform})
+    });
+
+
+    
+
 app.get("/admin", (req, res) => {
-    res.send(submissions);
+    res.render('admin', {orders});
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/index.html`)
+    res.render('index');
 
 });
 
-app.get(`/confirmation`, (req, res) => {
-   res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
-});
+
 
 app.listen(PORT,() => {
     console.log(`Server is running on http://localhost:${PORT}`);
